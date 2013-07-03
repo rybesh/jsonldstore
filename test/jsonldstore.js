1348aa136bab7913ba505c13e33fdcc43f308bec
Ryan Shaw <ryanshaw@unc.edu>
2013-07-02 21:30:56 -0400

    Implement GETting a graph.

--- test/jsonldstore.js
+++ test/jsonldstore.js
@@ -1,21 +1,26 @@
 var fs = require('fs')
   , http = require('http')
   , Writable = require('stream').Writable
+  , JSONLDTransform = require('jsonldtransform').JSONLDTransform
 
 module.exports =
-  { setUp: function(callback) {
-      this.load = function(path, callback) {
+  { setUp: function(done) {
+      var self = this
+      this.request = function(method, path, callback){
         var options = 
           { hostname: '0.0.0.0'
           , port: 8080
-          , method: 'POST'
-          , path: '/'
+          , method: method
+          , path: path
           }
-        fs.createReadStream(path).pipe(http.request(options, callback))  
+        return http.request(options, callback)
+      }
+      this.load = function(path, callback) {
+        fs.createReadStream(path).pipe(self.request('POST', '/', callback))  
       }
       this.nowhere = new Writable()
       this.nowhere._write = function(chunk, encoding, done) { done() }
-      callback()
+      done()
     }
 //------------------------------------------------------------------------------
   , 'loading via POST to /': function(test) {
@@ -26,6 +31,36 @@ module.exports =
         test.equal(res.headers.location, '/_graphs/test-graph-1')
         res.pipe(self.nowhere)
         test.done()
+      })
+    }
+//------------------------------------------------------------------------------
+  , 'GETting a graph': function(test) {
+      var self = this
+        , parsejsonld = new JSONLDTransform()
+      parsejsonld.on('error', function(e) {
+        test.ifError(e)
+      })
+      parsejsonld.on('graph', function(o) {
+        test.deepEqual(o, 
+          { '@id': '/_graphs/test-graph-1' 
+          , '@graph':
+              [ { "@id": "/topicnode/666" }
+              , { "@id": "http://www.wikidata.org/wiki/Q4115712" }
+              ]
+          })
+        test.done()
+      })
+      test.expect(1)
+      self.load('test/data/named_graph.json', function(res1) {
+        res1.pipe(self.nowhere)
+        self.request('GET', res1.headers.location, function(res2) {
+          if (res2.statusCode !== 200) {
+            test.fail(res2.statusCode, 200, null, '!==')
+            test.done()
+          } else {
+            res2.pipe(parsejsonld).pipe(self.nowhere)
+          }
+        }).end()
       })
     }
   }
