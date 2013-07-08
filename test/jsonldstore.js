41b0b320a06ddf17e27d415ab82500f33948c606
Ryan Shaw <ryanshaw@unc.edu>
2013-07-08 16:40:21 -0400

    Implement GET of graph's objects

--- test/jsonldstore.js
+++ test/jsonldstore.js
@@ -123,6 +123,46 @@ module.exports =
       })
     }
 //------------------------------------------------------------------------------
+  , 'GETting a graph\'s objects': function(test) {
+      var self = this
+        , expect
+        , parsejsonld = new JSONLDTransform()
+        , check = new Writable({objectMode:true})
+      expect = 
+        [ { "@id": "/topicnode/666"
+          , "@type": "http://www.wikidata.org/wiki/Q215627"
+          , "name": "Emma Goldman"
+          , "place of birth": "http://www.wikidata.org/wiki/Q4115712"
+          }
+        , { "@id": "http://www.wikidata.org/wiki/Q4115712"
+          , "@type": "http://www.wikidata.org/wiki/Q2221906"
+          , "name": "Kaunas"
+          }
+        ]
+      test.expect(expect.length)
+      parsejsonld.on('error', function(e) {
+        test.ifError(e)
+      })
+      check._write = function(chunk, encoding, callback) {
+        test.deepEqual(chunk, expect.shift())
+        callback()
+      }
+      check.on('finish', function(){
+        test.done()
+      })
+      self.load('test/data/named_graph.json', function(res1) {
+        self.consume(res1)
+        self.request('GET', res1.headers.location+'/objects', function(res2) {
+          if (res2.statusCode !== 200) {
+            test.fail(res2.statusCode, 200, null, '!==')
+            test.done()
+          } else {
+            res2.pipe(parsejsonld).pipe(check)
+          }
+        })
+      })
+    }
+//------------------------------------------------------------------------------
   , 'GET /graphs': function(test) {
       var self = this
         , buffer = ''
