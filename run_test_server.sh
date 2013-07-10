9d2ce0bd546bc36c95cab4992c53f27902381ac9
Ryan Shaw <ryanshaw@unc.edu>
2013-07-10 08:51:28 -0400

    Script for running a test server.

---
+++ run_test_server.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Use a bucket named 'test'...
+SERVER="./bin/jsonldstore -b test"
+
+# ...and pipe ouput to the bunyan logviewer. 
+LOGVIEW="./node_modules/bunyan/bin/bunyan -o short"
+
+# Run under nodemon if we have it.
+if command -v nodemon >/dev/null 2>&1 ; then
+    eval "nodemon $SERVER | $LOGVIEW"
+else
+    eval "$SERVER | $LOGVIEW"
+fi
+
+
+
+
